618a123ea29515019101fba3f5995ced23195f2c
supriyaanand <supriya.anandrao@gmail.com>
2015-03-07 23:38:52 +0530

    Initialize app

---
+++ db/migrate/20150307180120_create_schools.rb
@@ -0,0 +1,11 @@
+class CreateSchools < ActiveRecord::Migration
+  def change
+    create_table :schools do |t|
+      t.string :name
+      t.string :locality
+      t.string :address
+
+      t.timestamps null: false
+    end
+  end
+end
